e811b01ca2e5a384050583ebfade277da3a99cde
Youhei Shibata <yangping.dev@gmail.com>
2024-10-28 05:06:29 +0000

    first commit

---
+++ src/test/java/com/example/realwold/TestApplication.java
@@ -0,0 +1,9 @@
+package com.example.realwold;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+	public static void main(String[] args) {
+		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+	}
+}
